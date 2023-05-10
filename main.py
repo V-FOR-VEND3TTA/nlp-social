ea49045170448f36323b4ecd04a8ff6ae1b676cc
George Hlongwane <georgehlongwane8@gmail.com>
2023-05-10 08:51:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+""" Cleaning Text Steps
+1. Create a text file and take text from it
+2. Convert the letters into lowercase ('Apple' is not equal to 'apple')
+3. Remove punctuation marks e.g. !, ?, etc
+"""
